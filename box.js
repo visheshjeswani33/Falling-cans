28d74c242b56fbcf46eb3c3d22a02a3631254241
visheshjeswani33 <64632774+visheshjeswani33@users.noreply.github.com>
2020-09-08 13:51:06 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,40 @@
+    
+    class Box{
+    constructor(x, y, w, h) {
+    var options = {
+
+    restitution :0.4,
+    friction :0.0,
+
+
+    }
+    this.visibility = true;
+    this.body = Bodies.rectangle(x, y, w, h, options);
+    this.width = w;
+    this.height = h;
+    World.add(world, this.body);
+
+    }
+    display(){
+        
+    if (this.body.speed<5) {
+    var angle = this.body.angle;
+    var pos= this.body.position;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    rectMode(CENTER);
+    fill("blue");
+    rect(0,0,this.width, this.height);
+    pop();
+
+    }
+    
+    else{
+    World.remove(world, this.body);
+    push();
+    this.visibility =false;
+    pop();
+    }
+    }
+    }
